bdc71a761230c95d6081a5f05fdb7051cce2acba
Antelope <webantelopestudio@gmail.com>
2019-10-16 18:33:25 +0300

    upgrade task_12.js

--- 11-20/task_12.js
+++ 11-20/task_12.js
@@ -11,30 +11,60 @@
 //
 //   "\5ab\3cde" => [ "ab\3c", "d", "e" ]
 
-function decode(str) {
-  let result = [];
-  let pass = 0;
-
-  str.split('').forEach((element, index, array) => {
-    if (pass > 0) {
-      --pass;
-      return;
-    }
-
-    if (element === "\\") {
-      pass = array.slice(index).join('').match(/\d+/)[0];
-      result.push(array.slice(index + pass.length, index + +pass).join(''));
-      return;
-    }
-
-    result.push(element);
-  });
-
-  return result;
-}
+// function decode(str) {
+//
+//
+//
+//
+//
+//
+//
+//   let result = [];
+//   let pass = 0;
+//   console.log(str);
+//   str.split('').forEach((element, index, array) => {
+//     if (pass > 0) {
+//       --pass;
+//       return;
+//     }
+//
+//     if (element === "\\") {
+//       pass = array.slice(index).join('').match(/\d+/)[0];
+//       result.push(array.slice(++index + pass.length, index + +pass).join(''));
+//       +pass++;
+//       return;
+//     }
+//
+//     result.push(element);
+//   });
+//
+//   return result;
+// }
 
 console.log(decode('')); // []
 console.log(decode('abc')); // ['a','b','c']
 console.log(decode('\\1abc')); // ['a','b','c']
+console.log(decode('\\5abc\\1bca')); // ['3ab', 'c', '1', 'b', 'c']
 
-console.log(decode('\\1abc\\1bc')); // ['3ab', 'c', '1', 'b', 'c']
+
+function decode (string) {
+
+
+  // return string.split('').reduce((result, element, index, array) => {
+  //   if (result[0][0] > 0) {
+  //     --result[0][0];
+  //     return result;
+  //   }
+  //   if (element == '\\') {
+  //     console.log('INDEX', index);
+  //     let n = array.slice(index).join('').match(/\d+/)[0];
+  //     result[0].push(+n);
+  //     result[1].push(array.slice(index + 1 + n.length, index + 1 + +n).join(''));
+  //     return result;
+  //   }
+  //
+  //   result[1].push(element);
+  //
+  //   return result;
+  // }, [[], []]);
+}
