3ddfa88925c950e6541dc5d293575c968b8ed6cf
Antelope <webantelopestudio@gmail.com>
2019-11-10 19:03:00 +0300

    modified 01-FindTheLongestGap.js

--- newStyle/7 kyu/01-FindTheLongestGap.js
+++ newStyle/7 kyu/01-FindTheLongestGap.js
@@ -7,19 +7,19 @@
 // Write function gap(num) that, given a positive num, returns the length of its longest binary gap.
 // The function should return 0 if num doesn't contain a binary gap.
 
-const gap = num => {
-  let parsed = Number(num).toString(2).split(1);
-  let length = parsed.map(element => element.length);
-  let counter = Math.max.apply(null, length);
-  console.log(Number(num).toString(2).split(1), length, counter);
+// const gap = num => {
+//   let parsed = Number(num).toString(2).split(1);
 
-  return counter;
-}
+//   parsed.pop();
+
+//   let length = parsed.map(element => element.length);
+//   let counter = Math.max.apply(null, length);
+
+//   return counter;
+// }
 
 // or 
+// Shit code
+const gap = num => Math.max.apply(null, Number(num).toString(2).split(1).map(x => x.length));
 
-// const gap = num => Math.max.apply(null, Number(num).toString(2).split(1).map(x => x.length));
-
-
-
-console.log(gap('5848927'), (5848927).toString(2));
+console.log(gap('20'), (20).toString(2));
