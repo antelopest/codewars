a00ea069fe95733005ea90e3e267778ed24cda0a
antelopest <antelopest@gmail.com>
2020-08-09 00:11:05 +0300

    added new completed task

--- js/06_Int32ToIP_5_kyu.js
+++ js/06_Int32ToIP_5_kyu.js
@@ -47,4 +47,18 @@ console.log(
 )
 
 
+/*
+  OR USE BITWISE OPERATIONS
+
+  function int32ToIp(int32) {
+   return (
+            ((int32 >> 24) & 0xFF) + "." +
+            ((int32 >> 16) & 0xFF) + "." +
+            ((int32 >>  8) & 0xFF) + "." +
+            ((int32) & 0xFF)
+          );
+   }
+*/
+
+
 
