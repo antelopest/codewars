3d91064d265fbc6ef7c564dc711ad8c26fee1d95
s.belenko <antelopest@gmail.com>
2025-03-08 00:53:28 +0700

    001

--- javascript/001.js
+++ javascript/001.js
@@ -7,16 +7,23 @@
 const repeatStr = (num, str) => new Array(num).fill(str).join('');
 const repeatStr2 = (num, str) => str.repeat(num);
 
-console.log(repeatStr(5, "123"));
-console.log(repeatStr2(5, "123"));
-
 const tests = [
     {
         input: [5, "123"],
-        output: ["123123123123123"]
+        output: "123123123123123"
     },
     {
         input: [5, "Hello"],
-        output: ["HelloHelloHelloHelloHello"]
+        output: "HelloHelloHelloHelloHello"
     }
 ];
+
+const done = (input, output) => {
+    let done = repeatStr(...input) === output;
+
+    console.log(`${input[0]},${input[1]} | ${output} | ${done ? "DONE" : "FAIL"}`);
+}
+
+tests.forEach(test => {
+    done(test.input, test.output);
+})
